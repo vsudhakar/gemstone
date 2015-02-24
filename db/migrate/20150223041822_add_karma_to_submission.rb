d2e504227e088864f3706ca00c499f18e3f93afb
vsudhakar <visrut@suvithra.com>
2015-02-24 15:49:27 -0500

    edited migrations

--- db/migrate/20150223041822_add_karma_to_submission.rb
+++ db/migrate/20150223041822_add_karma_to_submission.rb
@@ -1,6 +1,6 @@
 class AddKarmaToSubmission < ActiveRecord::Migration
   def change
-    remove_column :submissions, :karma, :string
+    #remove_column :submissions, :karma, :string
     add_column :submissions, :karma, :integer
   end
 end
